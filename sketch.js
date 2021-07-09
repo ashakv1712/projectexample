235026660b7ca875a227c9d17dbc830f63b21089
ashakv1712 <74943973+ashakv1712@users.noreply.github.com>
2021-07-09 16:24:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var ball, ballImg;
+
+
+function preload(){
+
+}
+function setup() {
+  createCanvas(500, 500);
+
+  ball = createSprite(200,200,20,20);
+ 
+}
+
+function draw() {
+  background(50);
+
+ 
+  drawSprites();
+}
+
+
+
+
+
